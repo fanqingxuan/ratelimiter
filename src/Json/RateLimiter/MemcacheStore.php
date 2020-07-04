2a771ce621b32189e1713172907db2495a9e491a
范晓杰 <fxj@acewill.cn>
2020-07-04 22:13:50 +0800

    add memcache store

---
+++ src/Json/RateLimiter/MemcacheStore.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Json\RateLimiter;
+
+use \Memcache;
+
+class MemcacheStore {
+
+	private $memcache;
+	private $prefix = '';
+
+	public function __construct(Memcache $memcache,$prefix = '') {
+		$this->memcache = $memcache;
+		$this->setPrefix($prefix);
+	}
+	/**
+     * Retrieve an item from the cache by key.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $value = $this->memcache->get($this->prefix.$key);
+
+        return $value;
+    }
+
+    /**
+     * Store an item in the cache for a given number of seconds.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  int  $seconds
+     * @return bool
+     */
+    public function put($key, $value, $seconds)
+    {
+        return $this->memcache->set(
+            $this->prefix.$key, $value, MEMCACHE_COMPRESSED, $seconds
+        );
+    }
+
+    /**
+     * Store an item in the cache if the key doesn't exist.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  int  $seconds
+     * @return bool
+     */
+    public function add($key, $value, $seconds)
+    {
+        return $this->memcache->add(
+            $this->prefix.$key, $value, MEMCACHE_COMPRESSED, $seconds
+        );
+    }
+
+    /**
+     * Increment the value of an item in the cache.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return int|bool
+     */
+    public function increment($key, $value = 1)
+    {
+        return $this->memcache->increment($this->prefix.$key, $value);
+    }
+
+    /**
+     * Store an item in the cache indefinitely.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function forever($key, $value)
+    {
+        return $this->put($key, $value, 0);
+    }
+
+    /**
+     * Remove an item from the cache.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function forget($key)
+    {
+        return $this->memcache->delete($this->prefix.$key);
+    }
+
+     /**
+     * Set the cache key prefix.
+     *
+     * @param  string  $prefix
+     * @return void
+     */
+    public function setPrefix($prefix)
+    {
+        $this->prefix = ! empty($prefix) ? $prefix.':' : '';
+    }
+	
+}
